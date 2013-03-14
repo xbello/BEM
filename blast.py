db2f3384450cff50e77dda91fc5fe6fea01e2b1a
Xabi Bello <xbello@gmail.com>
2013-03-14 19:17:23 +0100

    Added defaults to blast.py

--- blast.py
+++ blast.py
@@ -33,14 +33,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs a blast.")
     parser.add_argument("--query", dest="query", required=True)
     parser.add_argument("--subject", dest="subject", required=True)
-    parser.add_argument("--evalue", dest="evalue",
+    parser.add_argument("--evalue", dest="evalue", default="5",
         help="e-value expressed as the power of ten, being 5 = 10^-5")
-    parser.add_argument("--penalty", dest="penalty")
-    parser.add_argument("--reward", dest="reward")
-    parser.add_argument("--gapopen", dest="gapopen")
-    parser.add_argument("--gapextend", dest="gapextend")
+    parser.add_argument("--penalty", dest="penalty", type=int, default=5)
+    parser.add_argument("--reward", dest="reward", type=int, default=5)
+    parser.add_argument("--gapopen", dest="gapopen", type=int, default=10)
+    parser.add_argument("--gapextend", dest="gapextend", type=int, default=10)
  
     args = parser.parse_args()
 
     span_cores(args.subject)
     format_db(args.subject)
+    blast(args.query, args.subject, args.evalue, args.penalty, args.reward,
+        args.gapopen, args.gapextend)
