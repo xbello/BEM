cb16fbec2313e15c701a75f9baf90d6731899f54
Xabi Bello <xbello@gmail.com>
2013-03-15 17:01:17 +0100

    Some changes and fresh start after mess

--- blast.py
+++ blast.py
@@ -12,27 +12,30 @@ output_path = config.get("paths", "output_db")
 
 def blast(query, subject, evalue, penalty, reward, gapopen, gapextend):
     #Run the blast
-    p = Process(target=utils.blast, args=(query,))
+    p = Process(target=utils.blastn, args=(query, subject, evalue, penalty,
+        reward, gapopen, gapextend,))
+    p.start()
+    p.join()
 
-def format_db(subject):
+def format_db(subject, db_type):
     #Format each db
     #assert subject exist
-    for core in CORES:
-        utils.format_database(
-            os.path.join(output_path, core, subject), db_type)
+    for core in range(CORES):
+        utils.format_database(subject, db_type, config)
 
     return True
 
 def span_cores(subject):
     #Span the subject across CPUs available
     assert os.path.isfile(subject), "File doesn't exist {0}".format(subject)
-    utils.multicore(subject, CORES)
-    return True
+    subjects = utils.multicore(subject, config, CORES)
+    return subjects
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs a blast.")
     parser.add_argument("--query", dest="query", required=True)
     parser.add_argument("--subject", dest="subject", required=True)
+    parser.add_argument("--dbtype", dest="dbtype", default="nucl")
     parser.add_argument("--evalue", dest="evalue", default="5",
         help="e-value expressed as the power of ten, being 5 = 10^-5")
     parser.add_argument("--penalty", dest="penalty", type=int, default=5)
@@ -42,7 +45,9 @@ if __name__ == "__main__":
  
     args = parser.parse_args()
 
-    span_cores(args.subject)
-    format_db(args.subject)
-    blast(args.query, args.subject, args.evalue, args.penalty, args.reward,
-        args.gapopen, args.gapextend)
+    spanned_subjects = span_cores(args.subject)
+    #format_db(args.subject, args.dbtype)
+    for subject in spanned_subjects:
+        format_db(subject, args.dbtype)
+        #blast(args.query, subject, args.evalue, args.penalty,
+        #    args.reward, args.gapopen, args.gapextend)
