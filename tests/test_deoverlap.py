6708cf7ff4d19b107d55018efe7f660744db4309
Xabier Bello <xbello@gmail.com>
2013-04-07 16:58:24 +0200

    Added tests for deoverlapping sort

---
+++ tests/test_deoverlap.py
@@ -0,0 +1,17 @@
+import deoverlap
+
+class testDeoverlap():
+    def setUp(self):
+        unsorted_file = open("test_deoverlap.txt", "r").readlines()
+        self.unsorted_elements = [x.split() for x in unsorted_file]
+
+        sorted_file = open("test_deoverlap_sorted.txt", "r").readlines()
+        self.sorted_elements = [x.split() for x in sorted_file]
+
+    def test_sort_by_score(self):
+        """Check if the sorting is doing the sort by score
+        """
+        deoverlap.sort_by_score(self.unsorted_elements)
+        assert self.unsorted_elements ==\
+            self.sorted_elements
+
