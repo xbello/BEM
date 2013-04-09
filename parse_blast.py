9c664cbc5f55b347fa5124823720b7420b8253f3
Xabier Bello <xbello@gmail.com>
2013-04-09 12:01:10 +0200

    Added WUBLAST capabilities to parser

--- parse_blast.py
+++ parse_blast.py
@@ -1,3 +1,36 @@
+
+def select_blaster_keys(splitted_line):
+    """list -> (dict)
+    
+    Given a line of tabular output, return the header keys of the blaster.
+    This is a line from WU-BLAST:
+     "Query    Subject 1.4e-11 1       71.62   437     520     307     307     172     59.04   59.04   18      18      21      23      -1      3651    3150    +1      396     892"
+    
+    22 columns divided by \t
+
+    This is a line from BLAST:
+     "Query       Subject      100.00  1027    0       0       1       1027    237     1263    0.0     1211"
+
+     12 columns divided by \t
+    """
+
+    WUBLAST_HSP_KEYS = HSP_keys = ["query", "subject", "e_val", "n_scores",
+        "score_norm", "score", "length", "idents", "matchs", "n_mismatch",
+        "identity", "pc_pos", "n_gaps", "gap_len", "subject_gaps",
+        "subject_gap_len", "query_frame", "query_start", "query_end",
+        "subject_frame", "subject_start", "subject_end"]
+
+    BLAST_HSP_KEYS = ["query", "subject", "identity", "length", "n_mismatch",
+        "n_gaps", "query_start", "query_end", "subject_start", "subject_end",
+        "e_val", "score"]
+
+    if len(splitted_line) == 22:
+        return WUBLAST_HSP_KEYS
+    elif len(splitted_line) == 12:
+        return BLAST_HSP_KEYS
+    else:
+        raise Exception, "BLAST output cannot be determined"
+
 def blast_table_consumer(blast_output_file):
     '''Consume a blast file, yielding a dict entry:
     HSP = {"query": str,
@@ -15,15 +48,13 @@ def blast_table_consumer(blast_output_file):
 
     The blast_output_file must come ready to be opened directly.'''
 
-    HSP_keys = ["query", "subject", "identity", "length", "n_mismatch",
-        "n_gaps", "query_start", "query_end", "subject_start", "subject_end",
-        "e_val", "score"] 
-
     with open(blast_output_file, "r") as blast_output:
         for line in blast_output:
             
             data = line.strip().split("\t")
-            HSP = dict.fromkeys(HSP_keys)
+
+            HSP_keys = select_blaster_keys(data)
+            HSP = {}
 
             for fl in ["identity", "e_val", "score"]:
                 HSP[fl] = float(data[HSP_keys.index(fl)])
@@ -44,6 +75,10 @@ def write_parsed(HSP_dict):
     HSP_dict["direction"] = "+"
     if HSP_dict["query_start"] > HSP_dict["query_end"]:
         HSP_dict["direction"] = "C"
+        #Swap the start and ending points
+        HSP_hold = HSP_dict["query_start"]
+        HSP_dict["query_start"] = HSP_dict["query_end"]
+        HSP_dict["query_end"] = HSP_hold
 
     line = []
     for column in header:
