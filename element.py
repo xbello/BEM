65ce3173dbd4a6383747ec5806b4a6f5b18d4595
Xabier Bello <xbello@gmail.com>
2013-07-27 11:04:32 +0200

    Started classing of Elements

---
+++ element.py
@@ -0,0 +1,17 @@
+class Element(object):
+    pass
+
+class Retroelement(Element):
+    pass
+
+class LTR(Retroelement):
+    pass
+
+class LINE(Retroelement):
+    pass
+
+class SINE(Retroelement):
+    pass
+
+class DNATransposon(Element):
+    pass
