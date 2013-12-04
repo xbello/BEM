a25c447346f5fd74da7fcddb175f6668197e260a
Xabi Bello <xbello@gmail.com>
2013-12-04 14:22:30 +0100

    This is ongoing work. Doesn't work

--- BEM/blast.py
+++ BEM/blast.py
@@ -1,25 +1,45 @@
+#!/usr/bin/env/ python
+"""Proxy for BLASTing sequences."""
+
 import argparse
 import ConfigParser
-from multiprocessing import Process, cpu_count
+import os
+from multiprocessing import cpu_count
+from subprocess import Popen, PIPE
 
+import config
 import utils
 
-config = ConfigParser.SafeConfigParser()
-config.read("config.cfg")
-output_path = config.get("paths", "output_db")
 
+def blastn(query, subject, config):
+    """Launch a blastn."""
+    num_threads = cpu_count()
 
-def blast(query, subject, evalue, word, penalty, reward, gapopen, gapextend):
-    """This is a docstring"""
-    #TODO: Pass the num-threads to blastn program
-    CORES = cpu_count()
-    print CORES
-    p = Process(
-        target=utils.blastn, args=(
-            query, subject, evalue, word,
-            penalty, reward, gapopen, gapextend,))
-    p.start()
-    p.join()
+    binary = os.path.join(
+        config.get("binaries", "blast"),
+        config.get("binaries", "blastn"))
+
+    output_db = config.get("paths", "output_db")
+    output_blast = config.get("paths", "output_blast")
+
+    input_path = config.get("paths", "input_path")
+
+    # Don't be too greedy, and lift one core to the user if there're many
+    if num_threads > 1:
+        num_threads -= 1
+
+    command = [
+        binary,
+        "-query", os.path.join(input_path, query),
+        "-subject", subject,
+        "-db", os.path.join(output_db, subject),
+        "-outfmt", "6"]
+
+    # TODO: add here all blast options
+
+    p = Popen(command)
+
+    p.communicate()
 
 
 def format_db(subject, db_type):
@@ -31,18 +51,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs a blast.")
     parser.add_argument("--query", dest="query", required=True)
     parser.add_argument("--subject", dest="subject", required=True)
-    parser.add_argument("--dbtype", dest="dbtype", default="nucl")
-    parser.add_argument(
-        "--evalue", dest="evalue", default="5",
-        help="e-value expressed as the power of ten, being 5 = 10^-5")
-    parser.add_argument("--word", dest="word", default=11)
-    parser.add_argument("--penalty", dest="penalty", type=int, default=-5)
-    parser.add_argument("--reward", dest="reward", type=int, default=5)
-    parser.add_argument("--gapopen", dest="gapopen", type=int, default=10)
-    parser.add_argument("--gapextend", dest="gapextend", type=int, default=10)
+    parser.add_argument("--strategy", dest="strategy", default="blastn")
+    parser.add_argument("--config", dest="config_file")
 
     args = parser.parse_args()
 
-    format_db(args.subject, args.dbtype)
-    #blast(args.query, subject, args.evalue, args.penalty,
-    #    args.reward, args.gapopen, args.gapextend)
+    config_values = config.get_config_file(config)
+
+    if args.strategy == "blastn":
+        format_db(args.subject, "nucl")
+        blastn(args.query,
+               args.subject,
+               config)
+
+    elif args.strategy == "tblastn":
+        format_db(args.subject, "prot")
+
