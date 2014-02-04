75f1acfafbc697819f93fa404a29972f56edb6a8
Xabi Bello <xbello@gmail.com>
2014-02-04 18:00:56 +0100

    Adjusted match behaviour.

--- BEM/match.py
+++ BEM/match.py
@@ -1,22 +1,37 @@
+"""Object to represent each match from a Blast hit."""
+
+
 class Match(object):
-    '''Represent a match between a query sequence and a subject.'''
+
+    """Represent a match between a query sequence and a subject."""
+
     def __init__(self, **kwargs):
+        self.query_name = kwargs.get("query_name", "")
+        self.subject_name = kwargs.get("subject_name", "")
         self.query = kwargs.get("query", (0, 0))
         self.subject = kwargs.get("subject", (0, 0))
         self.chromosome = kwargs.get("chromosome", "")
         self.orientation = kwargs.get("orientation", "+")  # Or "C"
 
+    def _same_family(self, other):
+        """Return True if self and other are the same match family."""
+        return ((self.query_name == other.query_name) and
+                (self.subject_name == other.subject_name))
+
     def __eq__(self, other):
-        return (self.query[0] == other.query[0]) and\
-            (self.subject[0] == other.subject[0])
+        return ((self.query[0] == other.query[0]) and
+                (self.subject[0] == other.subject[0]) and
+                (self._same_family(other)))
 
     def __gt__(self, other):
-        return (self.query[0] > other.query[0]) and\
-            (self.subject[0] > other.subject[0])
+        return ((self.query[0] > other.query[0]) and
+                (self.subject[0] > other.subject[0]) and
+                (self._same_family(other)))
 
     def __lt__(self, other):
-        return (self.query[0] < other.query[0]) and\
-            (self.subject[0] < other.subject[0])
+        return ((self.query[0] < other.query[0]) and
+                (self.subject[0] < other.subject[0]) and
+                (self._same_family(other)))
 
-    #TODO:
-    # Implement __add__ to join two matches?
+    def __add__(self, other):
+        pass
--- BEM/tests/test_match.py
+++ BEM/tests/test_match.py
@@ -1,35 +1,59 @@
+"""Tests for the match object."""
+
+
+from unittest import TestCase
+
 from BEM import match
 
-class testMatch():
+
+class testMatch(TestCase):
     def setUp(self):
         self.match1 = match.Match()
         self.match2 = match.Match()
-        self.match3 = match.Match(query = (10, 1000),
-            subject = (5000, 5990),
-            orientation = "C",
-            chromosome = "contig12345")
+        self.match3 = match.Match(query_name="Query_Name",
+                                  subject_name="Subject_Name",
+                                  query=(10, 1000),
+                                  subject=(5000, 5990),
+                                  orientation="C",
+                                  chromosome="contig12345")
 
     def test_loading_defaults(self):
-        """Test that match object can be created empty"""
-        assert self.match1.query == (0, 0)
-        assert self.match1.subject == (0, 0)
-        assert self.match1.orientation == "+"
-    
+        """Test that match object can be created empty."""
+        self.assertEqual(self.match1.query, (0, 0))
+        self.assertEqual(self.match1.subject, (0, 0))
+        self.assertEqual(self.match1.orientation, "+")
+
     def test_loading_match(self):
-        """Test that match object can handle assignment on creation"""
-        assert self.match3.query == (10, 1000)
-        assert self.match3.subject == (5000, 5990)
-        assert self.match3.orientation == "C"
-    
+        """Test that match object can handle assignment on creation."""
+        self.assertEqual(self.match3.query, (10, 1000))
+        self.assertEqual(self.match3.subject, (5000, 5990))
+        self.assertEqual(self.match3.orientation, "C")
+
     def test_eq(self):
-        """Test that two match objects are equal to join (starts in the same
-        point"""
-        assert self.match1 == self.match2
+        """Test that two match objects are equal to join.
+
+        (Starts in the same point).
+
+        """
+        self.match1.query_name = self.match2.query_name = "Query_1"
+        self.match1.subject_name = self.match2.subject_name = "Subject_1"
+        self.assertTrue(self.match1 == self.match2)
+        self.assertFalse(self.match1 == self.match3)
 
     def test_lt(self):
-        """Test that one match objects is lower than other (starts before)"""
-        assert self.match1 < self.match3
+        """Test that one match objects is lower than other (starts before)."""
+        self.match2.query = (10, 10)
+        self.match2.subject = (10, 10)
+        self.assertTrue(self.match1 < self.match2)
+        self.assertFalse(self.match2 < self.match1)
+        # If matches are not from the same family, return false
+        self.assertFalse(self.match1 < self.match3)
 
     def test_gt(self):
-        """Test that one match objects is greater than other (starts after)"""
-        assert self.match3 > self.match1
+        """Test that one match objects is greater than other (starts after)."""
+        self.match1.query = (20, 20)
+        self.match1.subject = (20, 20)
+        self.assertFalse(self.match1 < self.match2)
+        self.assertTrue(self.match2 < self.match1)
+        # If matches are not from the same family, return false
+        self.assertFalse(self.match3 > self.match1)
